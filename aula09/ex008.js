9f646e316ee468891c4ea0a20f28ec31f72db7e7
marcosimaocode <marco.simao13@gmail.com>
2024-12-12 15:59:11 +0000

    min 22 video 11

---
+++ aula09/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A Velocidade do seu carro é de ${vel} km/h`)
+
+if(vel > 60) { // Condição simples
+    console.log(`Velocidade ultrapassada. Multado/a!`)
+} 
+
+console.log(`Dirija sempre com cinto de Segurança!`)
---
+++ aula09/ex009.js
@@ -0,0 +1,7 @@
+var país = 'espanha'
+console.log(`Vivendo em ${país}.`)
+if (país == 'Portugal') {
+    console.log('Você é Português!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
